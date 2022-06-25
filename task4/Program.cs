1dec45b7016fd34bac2fec61f7ca10794a051b62
IraRu <irina.rurina@gmail.com>
2022-06-26 01:21:58 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число C");
+            int numberC = Convert.ToInt32(Console.ReadLine());
+            
+            if(numberA > numberB)
+            {
+                if(numberA > numberC)
+                    Console.WriteLine(numberA);
+                else
+                    Console.WriteLine(numberC);    
+            }
+            else
+            {
+                if(numberB > numberC)
+                    Console.WriteLine(numberB);
+                else
+                    Console.WriteLine(numberC);
+            }        
+        }
+    }
+}
