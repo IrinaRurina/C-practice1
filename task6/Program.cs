0ec22518d714c98ea645d95169185ab5bdaf9c99
IraRu <irina.rurina@gmail.com>
2022-06-26 01:50:38 +0300

    add task 6

---
+++ task6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int half1 = number / 2;
+            double half2 = (double) number / 2;
+            double difference = half2 - half1;
+
+            if(difference == 0)
+            {
+                Console.WriteLine("Четное");
+            }
+            else
+            {
+                Console.WriteLine("Нечетное");
+            }
+        }
+    }
+}
