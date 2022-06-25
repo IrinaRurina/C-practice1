dfefa4b50446b4821e2f7d888f3c0ab6771faddc
IraRu <irina.rurina@gmail.com>
2022-06-26 01:07:00 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+            
+            if(numberA > numberB){
+                int max = numberA;
+                int min = numberB;
+                Console.WriteLine("MAX = {0}, MIN = {1}", max, min);
+            }
+            else{
+                int max = numberB;
+                int min = numberA;
+                Console.WriteLine("MAX = {0}, MIN = {1}", max, min);
+            }
+        }
+    }
+}
