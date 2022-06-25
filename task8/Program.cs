392980f35517fe0cd6b84cedd5712fd89dd0deb8
IraRu <irina.rurina@gmail.com>
2022-06-26 02:01:29 +0300

    add task 8

---
+++ task8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int index = 2;
+            Console.WriteLine("Все четные числа от 0 до N:");
+
+            while(index <= N)
+            {
+                Console.WriteLine(index);
+                index = index + 2;
+            }
+        }
+    }
+}
