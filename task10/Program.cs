d793f59b5ae86eb31407efe4b7243400ed35ce61
IraRu <irina.rurina@gmail.com>
2022-06-30 21:53:23 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        void secondOutOfThree(int threeDigits) {
+            int second = (threeDigits % 100) / 10;
+            Console.WriteLine("Вторая цифра числа " + threeDigits + " это " + second);
+        }
+        secondOutOfThree(456);
+        secondOutOfThree(782);
+        secondOutOfThree(918);
+        }
+    }
+}
