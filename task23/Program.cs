675a386f178d172fe383ab43546ba1c572dcb569
IraRu <irina.rurina@gmail.com>
2022-07-02 23:59:14 +0300

    add task 23

---
+++ task23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] calculateCubes(int num)
+            {
+                int[] cubes = new int[num];
+                int i = 1;
+                while (i <= num)
+                {
+                    cubes[i-1] = i * i * i;  
+                    Console.WriteLine(cubes[i-1]);   
+                    i++;
+                }    
+                return cubes;              
+            }
+            
+            int[] result1 = calculateCubes(3); 
+            int[] result2 = calculateCubes(5);
+
+        }
+    }
+}
