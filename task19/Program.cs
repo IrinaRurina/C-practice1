09a3002fede2ee004806c8da62fe9a86f2ef6970
IraRu <irina.rurina@gmail.com>
2022-07-09 00:37:50 +0300

    question about Array.ConvertAll

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,7 +6,7 @@ namespace task19
     {
         static void Main(string[] args)
         {
-            void isPalindrome(int num)
+            /*void isPalindrome(int num)
             {
                 if(num / 10000 >=10 || num / 10000 < 1)
                 {
@@ -30,12 +30,33 @@ namespace task19
                 }
                 
             }
-            
             isPalindrome(14212);
             isPalindrome(23432);
             isPalindrome(12821);
             isPalindrome(2343);
             isPalindrome(128251);
+            */
+
+            bool isPalindrome(int[] arr)
+            {
+                bool result = false;
+                int N = arr.Length;
+                for (int i = 0; i < N/2; i++) 
+                {
+                    if (arr[i] != arr[N - i - 1])
+                    {
+                        Console.WriteLine("Not palindrome");
+                        return result;
+                    }
+                } 
+                result = true;
+                Console.WriteLine("Palindrome");
+
+                return result;
+            }
+            
+            int[] arr = Array.ConvertAll(Console.Readline().Split(''), int.Parse); // ОШИБКА! Пустая символьная константа
+            isPalindrome(arr);
         }
     }
 }
