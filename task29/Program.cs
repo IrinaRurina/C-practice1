cc9f32aa6b19762f3ad970775d62ed1d1071793c
IraRu <irina.rurina@gmail.com>
2022-07-10 00:59:33 +0300

    add task 29

---
+++ task29/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задать и вывести на экран массив из N элементов
+
+            int[] array(int N)
+            {
+                int [] result = new int[N];
+                for(int i = 0; i <N; i++)
+                {
+                    result[i] = (new Random()).Next(0,100);
+                    Console.Write(result[i] + " ");
+                }
+                Console.WriteLine();
+
+                return result;
+            }
+            array(8);
+        }
+    }
+}
