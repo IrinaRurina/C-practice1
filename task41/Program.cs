ee3e3728739cf02c07a02408dff832ee5ff1651f
IraRu <irina.rurina@gmail.com>
2022-07-15 23:58:16 +0300

    add task 41

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+            Console.WriteLine("Введите несколько положительных и отрицательных чисел через пробел");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+
+            // for (int i = 0; i < arr.Length; i++)
+            // {
+            //     Console.Write(arr[i] + " ");
+            // }
+            // Console.WriteLine();
+            
+            int positive = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0) {positive += 1;}
+            }
+            Console.WriteLine("Количество положительных чисел - " + positive);
+        }
+    }
+}
