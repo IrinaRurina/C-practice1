6d157f4a6b64bb4e2e97b42a6d90c03d661450b9
IraRu <irina.rurina@gmail.com>
2022-07-16 02:04:30 +0300

    style changes

--- task43/Program.cs
+++ task43/Program.cs
@@ -9,11 +9,11 @@ namespace task43
             // Напишите программу, которая найдет точку пересечения двух прямых y=k1*x+b1, y=k2*x+b2
             // Значения k1, b1, k2, b2 задаются пользователем
 
-            void linearCross(int k1, int b1, int k2, int b2)
+            void LinearCross(int k1, int b1, int k2, int b2)
             {
                 if (k1 == k2) 
                 {
-                    Console.WriteLine("Точки пересечения не существует - прямые параллельны");
+                    Console.WriteLine("Точки пересечения прямой y={0}*x+{1} и прямой y={2}*x+{3} не существует - прямые параллельны", k1, b1, k2, b2);
                 }
 
                 else
@@ -21,12 +21,12 @@ namespace task43
                     double x = (b2 - b1) / (k1 - k2);
                     double y = k1 * x + b1;
 
-                    Console.WriteLine("Координаты точки пересечения прямой y = {0} * x + {1} и прямой y = {2} * x + {3} - ({4},{5})", k1, b1, k2, b2, x, y);
+                    Console.WriteLine("Координаты точки пересечения прямой y={0}*x+{1} и прямой y={2}*x+{3} - ({4},{5})", k1, b1, k2, b2, x, y);
                 }
             }
-            linearCross(-2, -5, 3, 0);
-            linearCross(1, 3, -2, 12);
-            linearCross(2, 1, 2, 5);
+            LinearCross(-2, -5, 3, 0);
+            LinearCross(1, 3, -2, 12);
+            LinearCross(2, 1, 2, 5);
         }
     }
 }
