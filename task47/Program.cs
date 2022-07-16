4d39bbe5fda9a02a33dcf2033b72eda944d3811c
IraRu <irina.rurina@gmail.com>
2022-07-16 22:57:26 +0300

    add task 47

---
+++ task47/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+            
+            int M = Convert.ToInt32(Console.ReadLine());
+            int N = Convert.ToInt32(Console.ReadLine());
+            double [,] array = new double[M, N];
+
+            Random rnd = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i,j] = 10 * rnd.NextDouble();
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
